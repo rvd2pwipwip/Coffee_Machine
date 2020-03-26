77bfa52bf27ec6f3117615707630e7bcc1c6ddbc
llanuzo <llanuzo@stingray.com>
2020-03-26 15:11:37 -0400

    Clean up SvodCallable

--- Utils/src/main/java/com/stingray/qello/firetv/android/async/SvodCallable.java
+++ Utils/src/main/java/com/stingray/qello/firetv/android/async/SvodCallable.java
@@ -1,5 +1,7 @@
 package com.stingray.qello.firetv.android.async;
 
+import android.util.Log;
+
 import com.stingray.qello.firetv.android.async.requestmodel.TokenRequestBody;
 import com.stingray.qello.firetv.android.async.requestmodel.TokenResponse;
 import com.stingray.qello.firetv.android.event.AuthenticationStatusUpdateEvent;
@@ -28,12 +30,9 @@ public abstract class SvodCallable<T> extends BaseCommunicator implements Callab
         return get(path, Preferences.getString(PreferencesConstants.ACCESS_TOKEN));
     }
 
-    protected String get(String path, String accessToken) throws IOException {
-        HttpURLConnection urlConnection;
-        Response response;
-        boolean performRetry = false;
-
-        do {
+    protected String get(String path, String accessToken) {
+        Response response = performWithTokenRefresh(() -> {
+            HttpURLConnection urlConnection;
             URL url = new URL(createUrl(path));
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
@@ -43,60 +42,81 @@ public abstract class SvodCallable<T> extends BaseCommunicator implements Callab
                 urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
             }
 
-            response = new Response(urlConnection.getResponseCode(), getResponseBody(urlConnection));
-
-            if (response.getCode() == 401 && !performRetry) {
-                String refreshToken = Preferences.getString(PreferencesConstants.REFRESH_TOKEN);
-                boolean performLogout = true;
-
-                if (refreshToken != null) {
-                    TokenRequestBody tokenRequestBody = new TokenRequestBody(refreshToken, CLIENT_ID);
-                    TokenResponse tokenResponse = new TokenCallable(tokenRequestBody).call();
-                    if (tokenResponse != null) {
-                        Preferences.setString(PreferencesConstants.ACCESS_TOKEN, tokenResponse.getAccessToken());
-                        performRetry = true;
-                        performLogout = false;
-                    }
-                }
-
-                if (performLogout) {
-                    Preferences.setLoggedOutState();
-                    EventBus.getDefault().post(new AuthenticationStatusUpdateEvent(false));
-                    performRetry = true;
-                }
-            } else {
-                performRetry = false;
-            }
-
-        } while (performRetry);
+            return new Response(urlConnection.getResponseCode(), getResponseBody(urlConnection));
+        });
 
         return response.getBody();
     }
 
-    protected Response post(String path, String jsonBody, Map<String, String> additionalHeaders) throws IOException {
-        URL url = new URL(createUrl(path));
+    protected Response post(String path, String jsonBody, Map<String, String> additionalHeaders) {
 
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("POST");
-        urlConnection.setRequestProperty("Content-Type", "application/json");
-        urlConnection.setRequestProperty("x-client-id", CLIENT_ID);
+        return performWithTokenRefresh(() -> {
+            URL url = new URL(createUrl(path));
 
-        String accessToken = Preferences.getString(PreferencesConstants.ACCESS_TOKEN);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("x-client-id", CLIENT_ID);
 
-        if (accessToken != null && !accessToken.isEmpty()) {
-            urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
+            String accessToken = Preferences.getString(PreferencesConstants.ACCESS_TOKEN);
+
+            if (accessToken != null && !accessToken.isEmpty()) {
+                urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
+            }
+
+
+            for (Map.Entry<String, String> entry : additionalHeaders.entrySet()) {
+                urlConnection.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+
+            try (OutputStream os = urlConnection.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            return new Response(urlConnection.getResponseCode(), getResponseBody(urlConnection));
+        });
+    }
+
+    private Response performWithTokenRefresh(Callable<Response> responseCallable) {
+        try {
+            Response response;
+            boolean isRetrying = false;
+            do {
+                response = responseCallable.call();
+
+                if (response.getCode() == 401 && !isRetrying) {
+                    refreshTokenOrPerformLogout();
+                    isRetrying = true;
+                } else {
+                    isRetrying = false;
+                }
+
+            } while (isRetrying);
+
+            return response;
+        } catch (Exception e) {
+            Log.w(TAG, "Failed to get response. Returning default response", e);
+            return new Response();
+        }
+    }
+
+    private void refreshTokenOrPerformLogout() {
+        String refreshToken = Preferences.getString(PreferencesConstants.REFRESH_TOKEN);
+        boolean performLogout = true;
+
+        if (refreshToken != null) {
+            TokenRequestBody tokenRequestBody = new TokenRequestBody(refreshToken, CLIENT_ID);
+            TokenResponse tokenResponse = new TokenCallable(tokenRequestBody).call();
+            if (tokenResponse != null) {
+                Preferences.setString(PreferencesConstants.ACCESS_TOKEN, tokenResponse.getAccessToken());
+                performLogout = false;
+            }
         }
 
-
-        for (Map.Entry<String, String> entry : additionalHeaders.entrySet()) {
-            urlConnection.setRequestProperty(entry.getKey(), entry.getValue());
+        if (performLogout) {
+            Preferences.setLoggedOutState();
+            EventBus.getDefault().post(new AuthenticationStatusUpdateEvent(false));
         }
-
-        try (OutputStream os = urlConnection.getOutputStream()) {
-            byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
-        }
-
-        return new Response(urlConnection.getResponseCode(), getResponseBody(urlConnection));
     }
 }
