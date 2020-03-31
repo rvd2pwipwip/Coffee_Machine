c62b7c004cbe4165446dcb3e063d3bd81db1db3b
llanuzo <llanuzo@stingray.com>
2020-03-31 10:05:16 -0400

    Add jasons component in subscribtion page

--- LoginComponent/src/main/java/com/stingray/qello/android/firetv/login/fragments/PurchaseFragment.java
+++ LoginComponent/src/main/java/com/stingray/qello/android/firetv/login/fragments/PurchaseFragment.java
@@ -33,7 +33,7 @@ public class PurchaseFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.subscription_layout, container, false);
+        View view = inflater.inflate(R.layout.subscription_layout, container, false);
 
         purchaseItemsLayout = view.findViewById(R.id.purchase_items);
 
@@ -43,23 +43,35 @@ public class PurchaseFragment extends Fragment {
                 new SvodSubscription("sc.rec.yearly.sub.1", false, "YEARLY", "Yearly", true)
         );
 
-        for (SvodSubscription sub: svodSubscriptionList) {
+        for (SvodSubscription sub : svodSubscriptionList) {
             boolean isMonthly = sub.getRecurrence().equalsIgnoreCase("MONTHLY");
+            LinearLayout purchaseItemLayout;
+            if (isMonthly) {
+                // TODO Use in app purchase interface to get sku info
+                purchaseItemLayout = (LinearLayout) inflater.inflate(R.layout.purchase_item, purchaseItemsLayout, false);
 
-            // TODO Use in app purchase interface to get sku info
-            String price = (isMonthly) ? "11.99" : "99.99";
-            String comment = (isMonthly) ? "per month after trial" : "(No free trial)";
+                TextView recurrenceView = purchaseItemLayout.findViewById(R.id.recurrence);
+                recurrenceView.setText(sub.getRecurrenceTitle());
+                TextView priceTextView = purchaseItemLayout.findViewById(R.id.price_text);
+                priceTextView.setText("11.99");
+                TextView commentView = purchaseItemLayout.findViewById(R.id.comment);
+                commentView.setText("per month after trial");
+            } else {
+                purchaseItemLayout = (LinearLayout) inflater.inflate(R.layout.purchase_item_rebate, purchaseItemsLayout, false);
 
-            // TODO Toggle layout based on recurrence
-            LinearLayout purchaseItemLayout = (LinearLayout) inflater.inflate(R.layout.purchase_item, purchaseItemsLayout, false);
-
-            TextView recurrenceView = purchaseItemLayout.findViewById(R.id.recurrence);
-            recurrenceView.setText(sub.getRecurrenceTitle());
-            TextView priceTextView = purchaseItemLayout.findViewById(R.id.price_text);
-            priceTextView.setText(price);
-            TextView commentView = purchaseItemLayout.findViewById(R.id.comment);
-            commentView.setText(comment);
-
+                TextView recurrenceView = purchaseItemLayout.findViewById(R.id.recurrence);
+                recurrenceView.setText(sub.getRecurrenceTitle());
+                TextView priceTextView = purchaseItemLayout.findViewById(R.id.price_text);
+                priceTextView.setText("99.99$");
+                TextView originalPrice = purchaseItemLayout.findViewById(R.id.strikethrough_price);
+                originalPrice.setText("143.88$");
+                TextView commentView = purchaseItemLayout.findViewById(R.id.comment);
+                commentView.setText("No free trial");
+                TextView savingsTitle = purchaseItemLayout.findViewById(R.id.percentage_savings_title);
+                savingsTitle.setText("Save");
+                TextView savings = purchaseItemLayout.findViewById(R.id.percentage_savings);
+                savings.setText("30%");
+            }
             purchaseItemsLayout.addView(purchaseItemLayout);
         }
 
