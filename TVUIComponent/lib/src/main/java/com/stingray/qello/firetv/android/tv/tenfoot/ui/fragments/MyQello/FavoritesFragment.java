c193307615e93f3646d510c75765a129a5dc620e
metrudeau <metrudeau@stingray.com>
2020-04-20 22:27:03 -0400

    Update app with TvUIComponent likalise values

--- TVUIComponent/lib/src/main/java/com/stingray/qello/firetv/android/tv/tenfoot/ui/fragments/MyQello/FavoritesFragment.java
+++ TVUIComponent/lib/src/main/java/com/stingray/qello/firetv/android/tv/tenfoot/ui/fragments/MyQello/FavoritesFragment.java
@@ -24,7 +24,7 @@ public class FavoritesFragment extends BrowsePageFragment {
 
     @Override
     protected String getClearButtonText() {
-        return "Clear Favorites";
+        return getResources().getString(R.string.MyService_ClearFavorite);
     }
 
     @Override
