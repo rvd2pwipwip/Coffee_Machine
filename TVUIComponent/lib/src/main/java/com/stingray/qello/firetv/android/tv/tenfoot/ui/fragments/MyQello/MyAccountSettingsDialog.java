307b289e29a1292bef93cb3dcc8f233c4faa2601
llanuzo <llanuzo@stingray.com>
2020-05-06 13:56:12 -0400

    Change my account display information

--- TVUIComponent/lib/src/main/java/com/stingray/qello/firetv/android/tv/tenfoot/ui/fragments/MyQello/MyAccountSettingsDialog.java
+++ TVUIComponent/lib/src/main/java/com/stingray/qello/firetv/android/tv/tenfoot/ui/fragments/MyQello/MyAccountSettingsDialog.java
@@ -3,6 +3,7 @@ package com.stingray.qello.firetv.android.tv.tenfoot.ui.fragments.MyQello;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,11 +23,11 @@ public class MyAccountSettingsDialog extends AppInfoDialog {
 
     private static final String TAG = MyAccountSettingsDialog.class.getSimpleName();
 
-    private static final SimpleDateFormat ulDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-    private static final SimpleDateFormat printDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private final SimpleDateFormat ulDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    private final SimpleDateFormat printDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public void createFragment(final Activity activity, FragmentManager manager) {
-        super.createFragment(activity, manager,"MyAccountSettingsFragment");
+        super.createFragment(activity, manager, "MyAccountSettingsFragment");
     }
 
     protected SingleViewProvider getSingleViewProvider(Context context) {
@@ -37,65 +38,76 @@ public class MyAccountSettingsDialog extends AppInfoDialog {
 
         if (isLoggedIn) {
             String email = Preferences.getString(PreferencesConstants.STINGRAY_EMAIL);
-
             if (hasSubscription) {
-                // TODO Get the price and recurrence
-                String subscriptionEnd = Preferences.getString(PreferencesConstants.SUBSCRIPTION_END_DATE);
-
-                String subscriptionPrice = "9.99$";
-                String subscriptionRecurrence = "MONTHLY";
-
-                singleViewProvider = (context1, inflater, parent) -> {
-                    final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout, parent);
-
-                    TextView emailView = view.findViewById(R.id.my_account_email);
-                    emailView.setText(email);
-
-                    String formattedSubscriptionEnd;
-                    try {
-                        Date date = ulDateFormat.parse(subscriptionEnd);
-                        formattedSubscriptionEnd = printDateFormat.format(date);
-                    } catch (ParseException e) {
-                        formattedSubscriptionEnd = subscriptionEnd;
-                    }
-
-                    TextView subEndView = view.findViewById(R.id.my_account_sub_end);
-                    subEndView.setText(formattedSubscriptionEnd);
-
-                    TextView subRecurrenceView = view.findViewById(R.id.my_account_sub_rec);
-                    subRecurrenceView.setText(subscriptionRecurrence);
-
-                    TextView subPriceView = view.findViewById(R.id.my_account_sub_price);
-                    subPriceView.setText(subscriptionPrice);
-
-                    return view;
-                };
+                singleViewProvider = getLoggedInWithSubViewProvider(email);
             } else {
-                singleViewProvider = (context1, inflater, parent) -> {
-                    final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout_logged_nosub, parent);
-                    TextView emailView = view.findViewById(R.id.my_account_email);
-                    emailView.setText(email);
-
-                    Button freeTrialButton = view.findViewById(R.id.free_trial_button);
-                    freeTrialButton.setOnClickListener(v -> ContentBrowser.getInstance(mActivity)
-                            .switchToScreen(ContentBrowser.PURCHASE_SCREEN, null, null));
-
-                    return view;
-                };
+                singleViewProvider = getLoggedInWithoutSubViewProvider(email);
             }
         } else {
-            singleViewProvider = (context1, inflater, parent) -> {
-                final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout_guest, parent);
-
-                Button freeTrialButton = view.findViewById(R.id.free_trial_button);
-                freeTrialButton.setOnClickListener(v -> ContentBrowser.getInstance(mActivity)
-                        .switchToScreen(ContentBrowser.ACCOUNT_CREATION_SCREEN, null, null));
-
-                return view;
-            };
+            singleViewProvider = getLoggedOutViewProvider();
         }
 
         return singleViewProvider;
     }
 
+    private SingleViewProvider getLoggedInWithoutSubViewProvider(String email) {
+        return (context1, inflater, parent) -> {
+            final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout_logged_nosub, parent);
+            TextView emailView = view.findViewById(R.id.my_account_email);
+            emailView.setText(email);
+
+            Button freeTrialButton = view.findViewById(R.id.free_trial_button);
+            freeTrialButton.setOnClickListener(v -> ContentBrowser.getInstance(mActivity)
+                    .switchToScreen(ContentBrowser.PURCHASE_SCREEN, null, null));
+
+            return view;
+        };
+    }
+
+    private SingleViewProvider getLoggedInWithSubViewProvider(String email) {
+        String subscriptionEnd = Preferences.getString(PreferencesConstants.SUBSCRIPTION_END_DATE);
+        String formattedSubscriptionEndDate = null;
+
+        if (subscriptionEnd != null && !subscriptionEnd.isEmpty()) {
+            try {
+                Date date = ulDateFormat.parse(subscriptionEnd);
+                formattedSubscriptionEndDate = printDateFormat.format(date);
+            } catch (ParseException e) {
+                Log.e(TAG, "Failed to parse subscription end date", e);
+            }
+        }
+
+        final String endDateText = (formattedSubscriptionEndDate != null)
+                ? String.format(mActivity.getResources().getString(R.string.Account_PlanEnd), formattedSubscriptionEndDate)
+                : null;
+
+        return (context1, inflater, parent) -> {
+            final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout, parent);
+
+            TextView emailView = view.findViewById(R.id.my_account_email);
+            emailView.setText(email);
+
+            TextView subEndViewTextView = view.findViewById(R.id.my_account_subscription_end_date);
+            if (endDateText != null) {
+                subEndViewTextView.setText(endDateText);
+                subEndViewTextView.setVisibility(View.VISIBLE);
+            } else {
+                subEndViewTextView.setVisibility(View.GONE);
+            }
+
+            return view;
+        };
+    }
+
+    private SingleViewProvider getLoggedOutViewProvider() {
+        return (context1, inflater, parent) -> {
+            final View view = mActivity.getLayoutInflater().inflate(R.layout.my_account_layout_guest, parent);
+
+            Button freeTrialButton = view.findViewById(R.id.free_trial_button);
+            freeTrialButton.setOnClickListener(v -> ContentBrowser.getInstance(mActivity)
+                    .switchToScreen(ContentBrowser.ACCOUNT_CREATION_SCREEN, null, null));
+
+            return view;
+        };
+    }
 }
